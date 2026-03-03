d8bb9cd3c240f1cba4fa97d4bc351f51d1864a96
Anton Ivanov <anton.ivanov@cambridgegreys.com>
2026-03-03 09:47:09 +0000

    Update for testing, add unit tests
    Updated the module for testing, added unit tests using nose2
    
    Signed-off-by: Anton Ivanov <anton.ivanov@cambridgegreys.com>

---
+++ ompmultiprocessing/tests/test_omp.py
@@ -0,0 +1,95 @@
+#!/usr/bin/python3
+
+
+'''BPF Map functional test
+'''
+
+# pybpfmap, Copyright (c) 2023 RedHat Inc
+# pybpfmap, Copyright (c) 2023 Cambridge Greys Ltd
+
+# This source code is licensed under both the BSD-style license (found in the
+# LICENSE file in the root directory of this source tree) and the GPLv2 (found
+# in the COPYING file in the root directory of this source tree).
+# You may select, at your option, one of the above-listed licenses.
+
+
+from ompmultiprocessing import OMPProcessManager
+
+import os
+
+SIMPLE_MAP="simple.json"
+NUMA_MAP="numa.json"
+
+def runnable(arg, expected_places=None, expected_threads=0):
+    assert(expected_places == os.environ["OMP_PLACES"])
+    assert(expected_threads == int(os.environ["OMP_NUM_THREADS"]))
+
+def set_vllm_environment(env=None):
+    if env is None:
+        try:
+            del os.environ["VLLM_CPU_OMP_THREADS_BIND"]
+        except KeyError:
+            pass
+    else:
+        os.environ["VLLM_CPU_OMP_THREADS_BIND"] = env
+
+def test_simple():
+    '''Test Simple Topology'''
+    set_vllm_environment()
+    kwargs = dict()
+    manager = OMPProcessManager(strategy="nodes", smt=False, mock="simple.json", affinity=set(range(0,6)))
+    kwargs["expected_places"] = str(set(range(0,6)))
+    kwargs["expected_threads"] = 6
+    manager.run(runnable, None, **kwargs)
+
+def test_complex():
+    '''Test Complex Topology'''
+    set_vllm_environment()
+    kwargs = dict()
+    manager = OMPProcessManager(strategy="nodes", smt=False, mock="numa.json", affinity=set(range(0,96)))
+    kwargs["expected_places"] = str(set(range(0,32)))
+    kwargs["expected_threads"] = 32
+    manager.run(runnable, None, **kwargs)
+    kwargs["expected_places"] = str(set(range(32,64)))
+    kwargs["expected_threads"] = 32
+    manager.run(runnable, None, **kwargs)
+    kwargs["expected_places"] = str(set(range(64,96)))
+    kwargs["expected_threads"] = 32
+    manager.run(runnable, None, **kwargs)
+
+def test_mask():
+    '''Test Complex Topology with a mask'''
+    set_vllm_environment()
+    kwargs = dict()
+    manager = OMPProcessManager(strategy="nodes", smt=False, mock="numa.json", affinity=set(range(32,96)))
+    assert(len(manager.omp_places) == 2)
+    kwargs["expected_places"] = str(set(range(32,64)))
+    kwargs["expected_threads"] = 32
+    manager.run(runnable, None, **kwargs)
+    kwargs["expected_places"] = str(set(range(64,96)))
+    kwargs["expected_threads"] = 32
+    manager.run(runnable, None, **kwargs)
+
+def test_vllm_environment():
+    '''Test VLLM topology environment variables'''
+    set_vllm_environment("32-63|64-96")
+    kwargs = dict()
+    manager = OMPProcessManager(strategy="nodes", smt=False, mock="numa.json", affinity=set(range(0,96)))
+    assert(len(manager.omp_places) == 2)
+    kwargs["expected_places"] = str(set(range(32,64)))
+    kwargs["expected_threads"] = 32
+    manager.run(runnable, None, **kwargs)
+    kwargs["expected_places"] = str(set(range(64,96)))
+    kwargs["expected_threads"] = 32
+    manager.run(runnable, None, **kwargs)
+
+def test_vllm_environment_conflict():
+    '''Test VLLM topology environment variables'''
+    set_vllm_environment("32-63|64-96")
+    kwargs = dict()
+    manager = OMPProcessManager(strategy="nodes", smt=False, mock="numa.json", affinity=set(range(0,64)))
+    assert(len(manager.omp_places) == 1)
+    kwargs["expected_places"] = str(set(range(32,64)))
+    kwargs["expected_threads"] = 32
+    manager.run(runnable, None, **kwargs)
+
